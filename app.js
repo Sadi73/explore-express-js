f7c809f7f830a8a685275c48c7cdb9e9edf6881c
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2026-04-30 21:40:09 +0600

    this is an empty express project with basic layout

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const { userRouter } = require('./routes/users.routes');
+
+const app = express();
+
+app.use(cors());
+
+app.use(userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Server is running!')
+});
+
+
+// Handle 404 errors. This should be the last route handler, after all other routes have been defined.
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Not Found',
+        path: req.originalUrl,
+    });
+});
+// Handle 404 errors. This should be the last route handler, after all other routes have been defined.
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Not Found',
+        path: req.originalUrl,
+    });
+});
+
+// Handle server errors (500).
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: 'Internal Server Error',
+    });
+});
+
+module.exports = app;
---
+++ models/users.models.js
@@ -0,0 +1,6 @@
+const users = [
+    { id: 1, name: 'Sadi Muhammad Tanzim' },
+    { id: 2, name: 'Zarin Akter' },
+];
+
+module.exports = users;
